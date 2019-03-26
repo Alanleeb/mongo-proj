d207cacba2cd828d8c3ccbfc6cf66ab98788ecb3
Alan Bischoff <alanlbischoff@gmail.com>
2019-03-26 16:40:44 -0600

    form with post handler

--- models/Team.js
+++ models/Team.js
@@ -3,7 +3,8 @@ const mongoose = require ('mongoose')
 const Team = new mongoose.Schema({
     teamName: {type:String, trim:true, default:''},
     cityName: {type:String, trim:true, default:''},
-    good: {type:Boolean, default:' '}
+    good: {type:Boolean, default:' '},
+    conference: {type:String, trim:true, default:' '}
 })
 
 module.exports = mongoose.model("team", Team)
